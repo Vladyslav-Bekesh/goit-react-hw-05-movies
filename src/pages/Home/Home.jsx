5185c80940ddb1d417409f5d87d7ea0f772787e5
Vladyslav <dyadyavadya308@gmail.com>
2023-06-16 18:35:39 +0300

    error with movies

---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Home({ children }) {
+  return (
+    <>
+      <h1>Trendigs today</h1>
+      {children}
+    </>
+  );
+}
+
+export default Home;
+
+
---
+++ src/pages/Movies/Movies.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+
+function Movies(props) {
+  return (
+    <h2>asd</h2>
+  )
+}
+
+export default Movies;
+
+
