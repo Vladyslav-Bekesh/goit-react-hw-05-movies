5e044a3009b336145ecc3855fb9ad371382ca3b7
Vladyslav <dyadyavadya308@gmail.com>
2023-06-29 15:19:43 +0300

    some changes
    before goBackBtn

--- src/components/ActorsList/ActorsList.jsx
+++ src/components/ActorsList/ActorsList.jsx
@@ -3,16 +3,25 @@ import PropTypes from 'prop-types';
 import { ImgAdress } from '../../utils/fetch';
 
 function ActorsList({ actors }) {
+  const makeImgSrc = img => {
+    if (img) {
+      return `${ImgAdress}${img}`;
+    } else {
+      return 'https://via.placeholder.com/300x450';
+    }
+  };
+
   return (
     <>
       <h2>Actors</h2>
       <ul>
         {actors.cast.map(({ character, original_name, profile_path, id }) => {
-          const img = `${ImgAdress}${profile_path}`;
-
           return (
             <li key={id}>
-              <img src={img} alt="here can be actor, but he won`t take a pic)" />
+              <img
+                src={makeImgSrc(profile_path)}
+                alt="here can be actor, but somethig go wrong)"
+              />
               <p>Character: {character}</p>
               <p>Real name: {original_name}</p>
             </li>
--- src/components/GoBackButton/GoBackButton.jsx
+++ src/components/GoBackButton/GoBackButton.jsx
@@ -1,28 +1,15 @@
 import React from 'react';
-import { useLocation, useNavigate, Link } from 'react-router-dom';
+import { useLocation, useNavigate } from 'react-router-dom';
 
 function BackButton() {
   const location = useLocation();
   const navigate = useNavigate();
 
   const handleClick = () => {
-    if (
-      location.pathname.includes('cast') ||
-      location.pathname.includes('reviews')
-    ) {
-      console.log(location.pathname);
-      navigate('goit-react-hw-05-movies/movies/');
-      console.log(location.pathname);
-    } else {
-      window.history.back();
-    }
+    window.history.back();
   };
 
-  return (
-    <Link to="#" onClick={handleClick}>
-      <button>Back</button>
-    </Link>
-  );
+  return <button onClick={handleClick}>Back</button>;
 }
 
 export default BackButton;
